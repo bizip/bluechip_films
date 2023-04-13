515c158b70aa7cc6cacf3ce91d0cd0973e1dcbca
bizimungu pascal <52130946+bizip@users.noreply.github.com>
2023-04-13 23:55:10 +0000

    Add about page and news update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+const handleScroll = () => {
+  const navbar = document.querySelector('.header__nav');
+  const scrollTop = window.pageYOffset
+  || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+  const popupElement = document.getElementById('popup');
+  if (scrollTop >= document.querySelector('.about').offsetTop && popupElement.classList.contains('hidepopup')) {
+    navbar.classList.add('fixed');
+    if (popupElement.classList.contains('hidepopup')) {
+      document.getElementById('logo').style.display = 'block';
+    } else {
+      document.getElementById('logo').style.display = 'none';
+    }
+  } else {
+    document.getElementById('logo').style.display = 'block';
+    navbar.classList.remove('fixed');
+  }
+};
+window.addEventListener('scroll', handleScroll);
+
 document.getElementById('menu').addEventListener('click', (event) => {
   event.preventDefault();
   const overlay = document.getElementById('popup');
